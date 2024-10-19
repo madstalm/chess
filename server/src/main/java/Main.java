743256e6862451bef71ee20dfe2ad486a9b44e6c
madstalm <144950252+madstalm@users.noreply.github.com>
2024-10-19 13:16:31 -0600

    Wrote Service test for clear() method
    Also fixed the main method if statement

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        server.Server testServer = new server.Server();
-        testServer.run(8080);
+        if (args.length >= 2 && args[1].equals("sql")) {
+            boolean startWithSQL = true;
+            server.Server testServer = new server.Server(startWithSQL);
+            testServer.run(8080);
+        }
+        else {
+            server.Server testServer = new server.Server();
+            testServer.run(8080);
+        }
     }
 }
---
+++ server/src/test/java/service/ServiceTests.java
@@ -0,0 +1,49 @@
+package service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import service.AuthService;
+import service.GameService;
+import service.UserService;
+
+public class ServiceTests {
+    private static dataaccess.AuthDAO authDAO;
+    private static dataaccess.GameDAO gameDAO;
+    private static dataaccess.UserDAO userDAO;
+    private static AuthService authService;
+    private static GameService gameService;
+    private static UserService userService;
+
+    @BeforeAll
+    public static void init() {
+        authDAO = new dataaccess.MemoryAuthDAO();
+        gameDAO = new dataaccess.MemoryGameDAO();
+        userDAO = new dataaccess.MemoryUserDAO();
+        authService = new AuthService(authDAO);
+        gameService = new GameService(gameDAO);
+        userService = new UserService(userDAO);
+    }
+
+    @BeforeEach
+    public void setupService() {
+        authService = new AuthService(authDAO);
+        gameService = new GameService(gameDAO);
+        userService = new UserService(userDAO);
+    }
+
+    @Test
+    @DisplayName("Clear Positive")
+    public void clearService() throws Exception {
+        authService.clear();
+        gameService.clear();
+        userService.clear();
+    }
+}
