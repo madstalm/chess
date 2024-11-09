4dcb7228ae0f925508c81cf16fe7cb59bb4a8f18
madstalm <144950252+madstalm@users.noreply.github.com>
2024-11-08 19:04:16 -0700

    Wrote some initialize stuff for drawboard

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,5 +1,18 @@
 package ui;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+import chess.ChessGame;
+import chess.ChessBoard;
+import chess.ChessPiece;
 
 public class DrawBoard {
+    private ChessGame game;
+    private ChessBoard board;
 
+    public DrawBoard(ChessGame chessGame) {
+        this.game = chessGame;
+        this.board = this.game.getBoard();
+    }
 }
