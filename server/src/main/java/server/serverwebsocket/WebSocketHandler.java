68cb6a3bbfa1825d512318ca6cea60798aa69354
madstalm <144950252+madstalm@users.noreply.github.com>
2024-12-04 14:27:35 -0700

    WIP MakeMoveCommand (sub of UserGameCommand)
     - also WIP WebSocketHandler

--- server/src/main/java/server/serverwebsocket/WebSocketHandler.java
+++ server/src/main/java/server/serverwebsocket/WebSocketHandler.java
@@ -2,6 +2,9 @@ package server.serverwebsocket;
 
 import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.annotations.*;
+
+import java.io.IOException;
+
 import org.eclipse.jetty.websocket.api.*;
 
 import websocket.commands.*;
@@ -9,5 +12,35 @@ import websocket.messages.*;
 
 @WebSocket
 public class WebSocketHandler {
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        //check authorization for the command with the authtoken
+        switch (command.getCommandType()) {
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    //these will all likely throw exceptions
+    private void connect() {
+
+    }
+
+    private void makeMove() {
+
+    }
+
+    private void leave() {
+
+    }
+
+    private void resign() {
+
+    }
 
 }
