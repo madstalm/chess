30712589790c6e547133905f9b73669813f5ef78
madstalm <144950252+madstalm@users.noreply.github.com>
2024-12-03 22:01:14 -0700

    Work on ConnectionManager
    probably mostly done now, has to take in an additional gameID parameter so that people aren't getting blasted for every open game

--- server/src/main/java/server/serverwebsocket/ConnectionManager.java
+++ server/src/main/java/server/serverwebsocket/ConnectionManager.java
@@ -1,5 +1,39 @@
 package server.serverwebsocket;
 
-public class ConnectionManager {
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String username, Integer gameID, Session session) {
+        var connection = new Connection(username, gameID, session);
+        connections.put(username, connection);
+    }
+
+    public void remove(String username) {
+        connections.remove(username);
+    }
+
+    public void broadcast(String excludeUsername, Integer currentGameID, ServerMessage message) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if ((!c.username.equals(excludeUsername))&&(c.gameID.equals(currentGameID))) {
+                    c.send(message.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.username);
+        }
+    }
 }
